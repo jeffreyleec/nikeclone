813fb2ed777a5c765b3f985d823ba217a0cfb174
Jeffrey <jeffrey-lee@live.ca>
2024-04-28 23:27:49 -0700

    component skeleton files created and added to app file. components put into index for cleaner import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,45 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections/index";
+
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x padding-y">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
