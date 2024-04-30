e697d0d863423e2187799436d646146ddcc05b0f
Jeffrey <jeffrey-lee@live.ca>
2024-04-30 15:24:56 -0700

    sections added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
-      <img
-        src={iconURL}
-        className="w-5 h-5 ml-2 rounded-full"
-        alt="arrow right icon"
-      />
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="w-5 h-5 ml-2 bg-white rounded-full"
+        />
+      )}
     </button>
   );
 };
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { services } from "../constants";
+import { ServiceCard } from "../components";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="flex flex-wrap justify-center max-container gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
